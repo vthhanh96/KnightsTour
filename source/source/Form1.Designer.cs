177516cc45ef7aec01fd0cfd2a047d9732ec77d7
vthhanh <vthhanh96@gmail.com>
2018-05-15 09:27:10 +0700

    add algorithm

--- source/source/Form1.Designer.cs
+++ source/source/Form1.Designer.cs
@@ -28,11 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.panelChessBoard = new System.Windows.Forms.Panel();
             this.btnDrawChessBoard = new System.Windows.Forms.Button();
             this.txtNumber = new System.Windows.Forms.TextBox();
-            this.btnDrawHorse = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelChessBoard
@@ -45,9 +53,10 @@
             // 
             // btnDrawChessBoard
             // 
+            this.btnDrawChessBoard.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDrawChessBoard.Location = new System.Drawing.Point(625, 98);
             this.btnDrawChessBoard.Name = "btnDrawChessBoard";
-            this.btnDrawChessBoard.Size = new System.Drawing.Size(159, 44);
+            this.btnDrawChessBoard.Size = new System.Drawing.Size(191, 44);
             this.btnDrawChessBoard.TabIndex = 1;
             this.btnDrawChessBoard.Text = "Draw Chessboard";
             this.btnDrawChessBoard.UseVisualStyleBackColor = true;
@@ -58,31 +67,90 @@
             this.txtNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtNumber.Location = new System.Drawing.Point(625, 34);
             this.txtNumber.Name = "txtNumber";
-            this.txtNumber.Size = new System.Drawing.Size(159, 30);
+            this.txtNumber.Size = new System.Drawing.Size(191, 30);
             this.txtNumber.TabIndex = 2;
             // 
-            // btnDrawHorse
+            // label2
             // 
-            this.btnDrawHorse.Location = new System.Drawing.Point(625, 180);
-            this.btnDrawHorse.Name = "btnDrawHorse";
-            this.btnDrawHorse.Size = new System.Drawing.Size(159, 44);
-            this.btnDrawHorse.TabIndex = 1;
-            this.btnDrawHorse.Text = "Draw Horse";
-            this.btnDrawHorse.UseVisualStyleBackColor = true;
-            this.btnDrawHorse.Click += new System.EventHandler(this.btnDrawHorse_Click);
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(20, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nhập x";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(20, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Nhập y";
+            // 
+            // txtX
+            // 
+            this.txtX.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtX.Location = new System.Drawing.Point(131, 46);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(159, 30);
+            this.txtX.TabIndex = 2;
+            // 
+            // txtY
+            // 
+            this.txtY.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtY.Location = new System.Drawing.Point(131, 106);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(159, 30);
+            this.txtY.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtX);
+            this.groupBox1.Controls.Add(this.txtY);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(625, 175);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(323, 163);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Nhập tọa độ";
+            // 
+            // btnRun
+            // 
+            this.btnRun.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRun.Location = new System.Drawing.Point(625, 372);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(159, 44);
+            this.btnRun.TabIndex = 1;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(964, 582);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.txtNumber);
-            this.Controls.Add(this.btnDrawHorse);
+            this.Controls.Add(this.btnRun);
             this.Controls.Add(this.btnDrawChessBoard);
             this.Controls.Add(this.panelChessBoard);
             this.Name = "Form1";
             this.Text = "Knight\'s Tour";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -93,7 +161,13 @@
         private System.Windows.Forms.Panel panelChessBoard;
         private System.Windows.Forms.Button btnDrawChessBoard;
         private System.Windows.Forms.TextBox txtNumber;
-        private System.Windows.Forms.Button btnDrawHorse;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.TextBox txtY;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Timer timer;
     }
 }
 
--- source/source/Form1.cs
+++ source/source/Form1.cs
@@ -12,6 +12,16 @@ namespace source
 {
     public partial class Form1 : Form
     {
+        private Point currentPoint;
+        private Point nextPoint;
+        private int sizeOfChessBoard;
+        private Boolean isStop = false;
+
+        private Boolean[,] arrHorsePass;
+
+        private int[] availableX = { -1, 1, 2, 2, 1, -1, -2, -2 };
+        private int[] availableY = { -2, -2, -1, 1, 2, 2, 1, -1 };
+
         public Form1()
         {
             InitializeComponent();
@@ -26,18 +36,19 @@ namespace source
         {
             if (txtNumber.Text == "") return;
 
-            int number = int.Parse(txtNumber.Text);
+            sizeOfChessBoard = int.Parse(txtNumber.Text);
+            arrHorsePass = new Boolean[sizeOfChessBoard, sizeOfChessBoard];
             int size = panelChessBoard.Size.Width;
 
-            float width = (float)size / number;
+            float width = (float)size / sizeOfChessBoard;
 
             Graphics graphic = panelChessBoard.CreateGraphics();
             SolidBrush whiteBrush = new SolidBrush(Color.White);
             SolidBrush brownBrush = new SolidBrush(Color.SandyBrown);
 
-            for(int i = 0; i < number; i++)
+            for (int i = 0; i < sizeOfChessBoard; i++)
             {
-                for (int j = 0; j < number; j++)
+                for (int j = 0; j < sizeOfChessBoard; j++)
                     if (i % 2 == 0)
                     {
                         graphic.FillRectangle(j % 2 == 0 ? whiteBrush : brownBrush, new RectangleF(j * width, i * width, width, width));
@@ -49,35 +60,90 @@ namespace source
             }
         }
 
-        private void drawHorse()
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(txtX.Text);
+            int y = int.Parse(txtY.Text);
+
+            currentPoint = new Point(x, y);
+            drawHorse(currentPoint);
+            timer.Start();
+        }
+
+        private void drawHorse(Point point)
         {
             if (txtNumber.Text == "") return;
 
-            int number = int.Parse(txtNumber.Text);
             int size = panelChessBoard.Size.Width;
 
-            float width = (float)size / number;
+            float width = (float)size / sizeOfChessBoard;
 
             Graphics graphic = panelChessBoard.CreateGraphics();
             Image blHorse = Image.FromFile("img_horse_black.png");
-            Image whHorse = Image.FromFile("img_horse_gray.png");
-            for (int i = 0; i < number; i++)
-            {
-                for (int j = 0; j < number; j++)
-                    if (i % 2 != 0)
-                    {
-                        graphic.DrawImage(j % 2 == 0 ? blHorse : whHorse, new RectangleF(j * width, i * width, width, width));
-                    }
-                    else
-                    {
-                        graphic.DrawImage(j % 2 != 0 ? blHorse : whHorse, new RectangleF(j * width, i * width, width, width));
-                    }
-            }
+            Image grHorse = Image.FromFile("img_horse_gray.png");
+            graphic.DrawImage(isPass(point) ? grHorse : blHorse, new RectangleF(point.X * width, point.Y * width, width, width));
+
+            arrHorsePass[point.X, point.Y] = true;
         }
 
-        private void btnDrawHorse_Click(object sender, EventArgs e)
+        private Point findNextPoint(Point point)
         {
-            drawHorse();
+            if (countAvailableSteps(point) == 0)
+            {
+                isStop = true;
+                return new Point();
+            }
+
+            int minSteps = 9;
+            Point result = new Point();
+
+            for (int i = 0; i < 8; i++)
+            {
+                Point nextPoint = new Point(point.X + availableX[i], point.Y + availableY[i]);
+                if (!isInChessBoard(nextPoint) || isPass(nextPoint)) continue;
+
+                int availableSteps = countAvailableSteps(nextPoint);
+                if (availableSteps < minSteps)
+                {
+                    minSteps = availableSteps;
+                    result = nextPoint;
+                }
+            }
+            return result;
         }
+
+        private int countAvailableSteps(Point point)
+        {
+            int count = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                Point availablePoint = new Point(point.X + availableX[i], point.Y + availableY[i]);
+                if (isInChessBoard(availablePoint) && !isPass(availablePoint)) count++;
+            }
+            return count;
+        }
+
+        private Boolean isInChessBoard(Point point)
+        {
+            return !(point.X < 0 || point.Y < 0 || point.X >= sizeOfChessBoard || point.Y >= sizeOfChessBoard);
+        }
+
+        private Boolean isPass(Point point)
+        {
+            return arrHorsePass[point.X, point.Y];
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (isStop)
+            {
+                timer.Stop();
+                return;
+            }
+
+            drawHorse(currentPoint);
+            currentPoint = findNextPoint(currentPoint);
+            drawHorse(currentPoint);
+        } 
     }
 }
